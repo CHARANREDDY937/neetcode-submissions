60926661bb4656af641277bb9263f8bfeb16a0ae
CHARANREDDY937 <bannuru.charan.reddy@gmail.com>
2026-09-13 14:24:46 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int n[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            n[(s.charAt(i)-'a')]++;
+            n[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(n[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
